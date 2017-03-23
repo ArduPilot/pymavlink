c0ad1173ecaee4dc600693d838c919dd499e929a
Mark Whitehorn <kd0aij@gmail.com>
2017-03-23 16:37:24 -0600

    use sample counter to check for dropouts

--- tools/mavfft_int.py
+++ tools/mavfft_int.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 import numpy
 import pylab
 import matplotlib
+import matplotlib.pyplot as pyplot
 
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
@@ -24,10 +25,10 @@ def fft(logfile):
     print("Processing log %s" % filename)
     mlog = mavutil.mavlink_connection(filename)
 
-    data = {'ACC1.rate' : 1000,
+    data = {'ACC1.rate' : 900,
             'ACC2.rate' : 1600,
             'ACC3.rate' : 1000,
-            'GYR1.rate' : 1000,
+            'GYR1.rate' : 900,
             'GYR2.rate' :  800,
             'GYR3.rate' : 1000}
     for acc in ['ACC1','ACC2','ACC3']:
@@ -38,11 +39,16 @@ def fft(logfile):
             data[gyr+'.'+ax] = []
 
     # now gather all the data
+    timestamp1 = []
+    seqcnt1 = []
     while True:
         m = mlog.recv_match(condition=args.condition)
         if m is None:
             break
         type = m.get_type()
+        if type.startswith("ACC1"):
+            seqcnt1.append(m.SampleUS)
+            timestamp1.append(m.TimeUS)
         if type.startswith("ACC"):
             data[type+'.AccX'].append(m.AccX)
             data[type+'.AccY'].append(m.AccY)
@@ -51,6 +57,10 @@ def fft(logfile):
             data[type+'.GyrX'].append(m.GyrX)
             data[type+'.GyrY'].append(m.GyrY)
             data[type+'.GyrZ'].append(m.GyrZ)
+
+    # SampleUS is just a sample counter
+    ts = 1e-6 * numpy.array(timestamp1)
+    seqcnt = numpy.array(seqcnt1)
 
     print("Extracted %u data points" % len(data['ACC1.AccX']))
     
@@ -65,73 +75,129 @@ def fft(logfile):
     pylab.legend(loc='upper right')
     pylab.ylabel('m/sec/sec')
     pylab.subplots_adjust(left=0.06, right=0.95, top=0.95, bottom=0.16)
-    pylab.ion()
     pylab.show()
     currentAxes = preview.gca()
-    print(currentAxes.get_xlim())
-    
-    try:
-        wait = input('select data, then hit enter:')
-    except:
-        pass
-        
-    s_start = int(currentAxes.get_xlim()[0])
-    s_end = int(currentAxes.get_xlim()[1])
+    s_start = 0
+    s_end = len(timestamp1)-1
     n_samp = s_end - s_start
-    print('sample range: ', s_start, s_end)
-    print('N samples: ', n_samp)
-    
-    preview.canvas.set_window_title("FFT input data length: " + str(n_samp) + " samples")
-    pylab.xlabel('sample index : nsamples = ' + str(n_samp))
-    preview.savefig('acc1z.png')
+    currentAxes.set_xlim(s_start, s_end)
+
+    # outer loop for repeating time window selection
+    while True:
         
-    for msg in ['ACC1', 'GYR1']:
-        fftwin = pylab.figure()
-        fftwin.set_size_inches(12, 3, forward=True)
-        f_res = float(data[msg+'.rate']) / n_samp
-
-        if msg.startswith('ACC'):
-            prefix = 'Acc'
-            title = 'Accelerometer FFT'
-        else:
-            prefix = 'Gyr'
-            title = 'Gyro FFT'
+        while True:
+            print('select sample range for fft analysis')
+            preview.canvas.set_window_title('select sample range')
+            try:
+                s_start = input('start sample: ')
+                s_end = input('end sample: ')
+                currentAxes.set_xlim(s_start, s_end)
+            except:
+                break
             
-        max_fft = 0
-        abs_fft = [0,0,0]
-        index = 0
-        for axis in ['X', 'Y', 'Z']:
-            field = msg + '.' + prefix + axis
-            d = data[field][s_start:s_end]
-            d = numpy.array(d)
-            freq  = numpy.fft.rfftfreq(len(d), 1.0 / data[msg+'.rate'])
-
-            if len(d) == 0:
-                continue
-            avg = numpy.sum(d) / len(d)
-            d -= avg
-            d_fft = numpy.fft.rfft(d)
-            abs_fft[index] = numpy.abs(d_fft)
-            thismax = numpy.max(abs_fft[index])
-            if (max_fft < thismax):
-                max_fft = thismax
-            index += 1
+        # process selected samples
+        s_start = int(currentAxes.get_xlim()[0])
+        s_end = int(currentAxes.get_xlim()[1])
+        n_samp = s_end - s_start
+        print('sample range: ', s_start, s_end)
+        print('N samples: ', n_samp)
+        
+        # check for dropouts: (delta > 1)
+        deltas = numpy.diff(seqcnt[s_start:s_end])
+        print('ndeltas: ', len(deltas))
+        duration = ts[s_end] - ts[s_start]
+        print('duration: {0:.3f} seconds'.format(duration))
+        avg_rate = float(n_samp-1) / duration
+        print('average sample rate: {0:.0f} Hz'.format(avg_rate))
+        ts_mean = numpy.mean(deltas) 
+        print('mean sample interval: ', '{0:.2f}'.format(ts_mean))
+        print('std: ', '{0:.0f}'.format(numpy.std(deltas)))
+        
+        title = 'FFT input: {0:s} ACC1[{1:d}:{2:d}], {3:d} samples'.format(logfile, s_start, s_end, n_samp)
+        pylab.xlabel('sample index : nsamples: {0:d}, avg rate: {1:.0f} Hz'.format(n_samp, avg_rate))
+        preview.canvas.set_window_title(title)
+        preview.savefig('acc1z.png')
             
-        for index in range(0,3):
-            db_fft = 20 * numpy.log10(abs_fft[index] / max_fft)
-            pylab.plot( freq, db_fft, label=field )
+        drop_lens = []
+        drop_times = []
+        intvl_count = [0]
+        interval = 0
+        for i in range(0, len(deltas)):
+            if (deltas[i] < 1.5 * ts_mean):
+                interval += 1
+                intvl_count.append(interval)
+            else:
+                interval += 2
+                intvl_count.append(interval)
+                drop_lens.append(deltas[i])
+                drop_times.append(ts[s_start+i])
+                #print('dropout at sample {0}: length {1:.2f} msec'.format(i, 1e3*deltas[i]))
+        
+        print('{0:d} sample intervals > {1:.3f} msec'.format(len(drop_lens), 1.5 * ts_mean))
 
-        fftwin.canvas.set_window_title(title)
-        fftwin.gca().set_ylim(-90, 0)
-        pylab.legend(loc='upper right')
-        pylab.xlabel('Hz : resolution = ' + '{0:.3f}'.format(f_res))
-        pylab.ylabel('dB')
-        pylab.subplots_adjust(left=0.06, right=0.95, top=0.95, bottom=0.16)
-        fftwin.savefig(msg + '_fft.png')
+        for msg in ['ACC1', 'GYR1']:
+            fftwin = pylab.figure()
+            fftwin.set_size_inches(12, 3, forward=True)
+            f_res = float(data[msg+'.rate']) / n_samp
+    
+            if msg.startswith('ACC'):
+                prefix = 'Acc'
+                title = 'Accelerometer FFT [{0:d}:{1:d}]'.format(s_start, s_end)
+            else:
+                prefix = 'Gyr'
+                title = 'Gyro FFT [{0:d}:{1:d}]'.format(s_start, s_end)
+                
+            max_fft = 0
+            abs_fft = {}
+            index = 0
+            for axis in ['X', 'Y', 'Z']:
+                field = msg + '.' + prefix + axis
+                d = data[field][s_start:s_end]
+                if len(d) == 0:
+                    continue
+    
+                d = numpy.array(d)
+                freq  = numpy.fft.rfftfreq(len(d), 1.0 / data[msg+'.rate'])
+                # remove mean
+                avg = numpy.mean(d)
+                d -= avg
+                # transform
+                d_fft = numpy.fft.rfft(d)
+                abs_fft[axis] = numpy.abs(d_fft)
+                # remember the max over all axes
+                thismax = numpy.max(abs_fft[axis])
+                if (max_fft < thismax):
+                    max_fft = thismax
+                index += 1
+                
+            for axis in ['X', 'Y', 'Z']:
+                # scale to 0dB = max
+                field = msg + '.' + prefix + axis
+                db_fft = 20 * numpy.log10(abs_fft[axis] / max_fft)
+                pylab.plot( freq, db_fft, label=field )
+    
+            fftwin.canvas.set_window_title(title)
+            fftwin.gca().set_ylim(-90, 0)
+            pylab.legend(loc='upper right')
+            pylab.xlabel('Hz : resolution = ' + '{0:.3f}'.format(f_res))
+            pylab.ylabel('dB')
+            pylab.subplots_adjust(left=0.06, right=0.95, top=0.95, bottom=0.16)
+            fftwin.savefig(msg + '_fft.png')
+        
+        try:
+            selection = raw_input('q to proceed to next file, anything else to select a new range: ')
+            print(selection)
+        except:
+            continue
 
+        if (selection == 'q'): 
+            break
+
+pylab.ion()
 for filename in args.logs:
     fft(filename)
 
+print('type ctrl-c to close windows and exit')
 pylab.ioff()
 pylab.show()
 
