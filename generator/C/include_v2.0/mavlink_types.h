472c53ce343f7f8910c26243dd3058eb060c2f26
matt on dell15 <matt@matthewgong.com>
2017-06-19 14:54:18 +0800

    fix macros for compiling in MDK5 with armcc

--- generator/C/include_v2.0/mavlink_types.h
+++ generator/C/include_v2.0/mavlink_types.h
@@ -15,6 +15,10 @@ namespace mavlink {
 // Macro to define packed structures
 #ifdef __GNUC__
   #define MAVPACKED( __Declaration__ ) __Declaration__ __attribute__((packed))
+#elif defined __arm__
+  #pragma anon_unions
+  #pragma pack(push, 1)
+  #define MAVPACKED( __Declaration__ ) __Declaration__	
 #else
   #define MAVPACKED( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
 #endif
