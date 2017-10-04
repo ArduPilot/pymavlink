22ac23974993f1906bc9c9d034d871b0c2d8d1c9
Matthew Gong <matt@matthewgong.com>
2017-10-04 10:48:03 +0800

    modify macros for both MDK v4/v5

--- generator/C/include_v2.0/mavlink_types.h
+++ generator/C/include_v2.0/mavlink_types.h
@@ -17,8 +17,7 @@ namespace mavlink {
   #define MAVPACKED( __Declaration__ ) __Declaration__ __attribute__((packed))
 #elif defined __ARMCC_VERSION
   #pragma anon_unions
-  #pragma pack(push, 1)
-  #define MAVPACKED( __Declaration__ ) __Declaration__	
+  #define MAVPACKED( __Declaration__ ) _Pragma("pack(push,1)") __Declaration__ _Pragma("pack(pop)")
 #else
   #define MAVPACKED( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
 #endif
