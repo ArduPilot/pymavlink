b0f2f75ef260910472ec5819360ce6f5f4202fbc
Hunter N. Morgan <hunterm522@gmail.com>
2017-11-29 11:40:12 -0600

    java: format java code and add javadoc

--- generator/java/lib/Messages/MAVLinkMessage.java
+++ generator/java/lib/Messages/MAVLinkMessage.java
@@ -10,14 +10,13 @@ import java.io.Serializable;
 
 import com.MAVLink.MAVLinkPacket;
 
+/**
+ * Common interface for all MAVLink messages
+ */
 public abstract class MAVLinkMessage implements Serializable {
     private static final long serialVersionUID = -7754622750478538539L;
     // The MAVLink message classes have been changed to implement Serializable, 
     // this way is possible to pass a mavlink message trought the Service-Acctivity interface
-    
-    /**
-     *  Simply a common interface for all MAVLink Messages
-     */
     
     public int sysid;
     public int compid;
