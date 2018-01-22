db150aa42e2a00bedfe74a5eab21d52266323b86
pietrodn <powerpdn@gmail.com>
2018-01-22 19:27:53 +0100

    python3 fix: convert parameter name to bytes

--- mavutil.py
+++ mavutil.py
@@ -406,9 +406,9 @@ class mavfile(object):
         '''initiate fetch of one parameter'''
         try:
             idx = int(name)
-            self.mav.param_request_read_send(self.target_system, self.target_component, "", idx)
+            self.mav.param_request_read_send(self.target_system, self.target_component, b'', idx)
         except Exception:
-            self.mav.param_request_read_send(self.target_system, self.target_component, name, -1)
+            self.mav.param_request_read_send(self.target_system, self.target_component, name.encode('utf8'), -1)
 
     def time_since(self, mtype):
         '''return the time since the last message of type mtype was received'''
