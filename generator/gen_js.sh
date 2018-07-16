7a4600ebb8277eab67a411d55d464f45c562d48d
Hamish Willee <hamishwillee@gmail.com>
2018-07-16 14:24:10 +1000

    Remove MAVLink v0.9 support from gen_js.sh

--- generator/gen_js.sh
+++ generator/gen_js.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for protocol in 0.9 1.0; do
+for protocol in 1.0; do
  for xml in ../../message_definitions/v$protocol/*.xml; do
      base=$(basename $xml .xml)
      mkdir -p javascript/implementations/mavlink_${base}_v${protocol}
