25e4f3b377291fe653f903ca82bae8320d8ab530
Peter Barker <pbarker@barker.dropbear.id.au>
2018-11-26 12:22:33 +1100

    mavparmdiff.py: add option to not exclude some parameters from comparison

--- tools/mavparmdiff.py
+++ tools/mavparmdiff.py
@@ -9,6 +9,16 @@ from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
 parser.add_argument("file1", metavar="FILE1")
 parser.add_argument("file2", metavar="FILE2")
+parser.add_argument("--use-excludes",
+                    help="exclude volatile and similar parameters",
+                    default=True,
+                    action='store_true',
+                    dest='use_excludes')
+parser.add_argument("--no-excludes",
+                    help="include volatile and similar parameters",
+                    default=True,
+                    action='store_false',
+                    dest='use_excludes')
 args = parser.parse_args()
 
 file1 = args.file1
@@ -16,6 +26,6 @@ file2 = args.file2
 
 p1 = mavparm.MAVParmDict()
 p2 = mavparm.MAVParmDict()
-p1.load(file2)
-p1.diff(file1)
+p1.load(file2, use_excludes=args.use_excludes)
+p1.diff(file1, use_excludes=args.use_excludes)
 
