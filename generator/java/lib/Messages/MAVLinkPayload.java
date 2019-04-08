b228185dce8d114f9ad28f3a32e4819ad7284347
Rob Ratcliff <rob@futuretek.com>
2019-04-08 17:35:52 -0500

    Add support for doubles

--- generator/java/lib/Messages/MAVLinkPayload.java
+++ generator/java/lib/Messages/MAVLinkPayload.java
@@ -138,6 +138,10 @@ public class MAVLinkPayload {
     public float getFloat() {
         return Float.intBitsToFloat(getInt());
     }
+
+    public double getDouble() {
+        return Double.longBitsToDouble(getLong());
+    }   
     
     public void putByte(byte data) {
         add(data);
@@ -200,6 +204,10 @@ public class MAVLinkPayload {
 
     public void putFloat(float data) {
         putInt(Float.floatToIntBits(data));
+    }
+
+    public void putDouble(double data) {
+        putLong(Double.doubleToLongBits(data));
     }
 
 }
