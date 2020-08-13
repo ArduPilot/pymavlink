13a1c1cea4fad49f16f443c36cd2d054271ffc75
Andrew Tridgell <andrew@tridgell.net>
2020-08-13 18:15:04 +1000

    mavexpression: support conditional expressions
    use expression{condition}
    
    example:
    
    graph BATTERY_STATUS.current_battery*0.01{BATTERY_STATUS.id==0} BATTERY_STATUS.current_battery*0.01{BATTERY_STATUS.id==1}

--- mavexpression.py
+++ mavexpression.py
@@ -23,8 +23,21 @@ if home is not None:
         mavuser = imp.load_source('pymavlink.mavuser', extra)
         from pymavlink.mavuser import *
 
-def evaluate_expression(expression, vars):
+def evaluate_expression(expression, vars, nocondition=False):
     '''evaluation an expression'''
+    # first check for conditions which take the form EXPRESSION{CONDITION}
+    if expression[-1] == '}':
+        startidx = expression.rfind('{')
+        if startidx == -1:
+            return None
+        condition=expression[startidx+1:-1]
+        expression=expression[:startidx]
+        try:
+            v = eval(condition, globals(), vars)
+        except Exception:
+            return None
+        if not nocondition and not v:
+            return None
     try:
         v = eval(expression, globals(), vars)
     except NameError:
