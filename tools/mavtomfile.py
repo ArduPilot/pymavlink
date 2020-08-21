2660a0e298c1ed47ccbc1961266620ffdc90f97f
Mikel Graham <mikel.graham@gtri.gatech.edu>
2020-08-21 16:16:43 -0400

    mavtomfile: correct python2/python3 compatability issue
    Includes a python version check such that unicode values are
    properly converted in python 2.

--- tools/mavtomfile.py
+++ tools/mavtomfile.py
@@ -8,10 +8,13 @@ from builtins import range
 
 import os
 import re
+import sys
 from pymavlink import mavutil
 
 def process_tlog(filename):
     '''convert a tlog to a .m file'''
+
+    is_py2 = sys.version_info < (3,0)
 
     print("Processing %s" % filename)
 
@@ -62,6 +65,11 @@ def process_tlog(filename):
             f.write("%s.columns = {'timestamp'" % mtype)
             for field in fieldnames:
                 val = getattr(m, field)
+
+                if is_py2:
+                    if isinstance(val,unicode): # NOQA
+                        val = str(val)
+
                 if not isinstance(val, str):
                     if type(val) is not list:
                         f.write(",'%s'" % field)
@@ -74,6 +82,11 @@ def process_tlog(filename):
         f.write("%s.data(%u,:) = [%f" % (mtype, type_counters[mtype], m._timestamp))
         for field in m._fieldnames:
             val = getattr(m, field)
+
+            if is_py2:
+                if isinstance(val,unicode): # NOQA
+                    val = str(val)
+
             if not isinstance(val, str):
                 if type(val) is not list:
                     f.write(",%.20g" % val)
