58dae56d6636e44aedfe76a487e011b0275361bb
Christian Clauss <cclauss@me.com>
2020-08-21 09:34:59 +0200

    Travis CI: Upgrade Node.js and Python
    @peterbarker

--- generator/mavcrc.py
+++ generator/mavcrc.py
@@ -30,9 +30,11 @@ class x25crc(object):
         '''add in some more bytes'''
         accum = self.crc
         import array
-        bytes = array.array('B')
-        try:
-            bytes.fromstring(buf)
-        except AttributeError:  # Python >= 3.9
-            bytes.frombytes(buf.encode())
-        self.accumulate(bytes)
+        bytes_array = array.array('B')
+        try:  # if buf is bytes
+            bytes_array.frombytes(buf)
+        except TypeError:  # if buf is str
+            bytes_array.frombytes(buf.encode())
+        except AttributeError:  # Python < 3.2
+            bytes_array.fromstring(buf)
+        self.accumulate(bytes_array)
