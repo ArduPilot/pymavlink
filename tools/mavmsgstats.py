33bc90ef2df11d99cb1898a852096f16535fa591
Andrew Tridgell <andrew@tridgell.net>
2020-11-02 21:24:20 +1100

    mavmsgstats: added categories

--- tools/mavmsgstats.py
+++ tools/mavmsgstats.py
@@ -5,6 +5,7 @@ show stats on messages in a log
 '''
 
 import os
+import fnmatch
 
 from argparse import ArgumentParser
 parser = ArgumentParser(description=__doc__)
@@ -14,34 +15,57 @@ args = parser.parse_args()
 
 from pymavlink import mavutil
 
-REPLAY_MSGS = [ 'RFRH', 'RFRF', 'REV2', 'RSO2', 'RWA2', 'REV3', 'RSO3', 'RWA3',
-    'REY3', 'RFRN', 'RISH', 'RISI', 'RISJ', 'RBRH', 'RBRI', 'RRNH', 'RRNI',
-    'RGPH', 'RGPI', 'RGPJ', 'RASH', 'RASI', 'RBCH', 'RBCI', 'RVOH', 'RMGH',
-    'RMGI']
-    
+categories = {
+    'EKF2' : ['NK*'],
+    'EKF3' : ['XK*'],
+    'SENSORS' : ['IMU*', 'BAR*', 'GPS*', 'RFND', 'ACC', 'GYR' ],
+    'RC' : ['RCIN', 'RCOU' ],
+    'TUNING' : ['RATE', 'PID*', 'CTUN', 'NTUN', 'ATT', 'PSC'],
+    'SYSTEM' : ['MAV', 'BAT*', 'EV', 'CMD', 'MODE'],
+    'REPLAY' : [ 'RFRH', 'RFRF', 'REV2', 'RSO2', 'RWA2', 'REV3', 'RSO3', 'RWA3', 'RMGI',
+                 'REY3', 'RFRN', 'RISH', 'RISI', 'RISJ', 'RBRH', 'RBRI', 'RRNH', 'RRNI',
+                 'RGPH', 'RGPI', 'RGPJ', 'RASH', 'RASI', 'RBCH', 'RBCI', 'RVOH', 'RMGH' ],
+}
 
 def show_stats(logfile):
     '''show stats on a file'''
     print("Processing log %s" % filename)
     mlog = mavutil.mavlink_connection(filename)
+    sizes = {}
     total_size = 0
-    replay_total_size = 0
     names = mlog.name_to_id.keys()
     pairs = []
+
+    for name in names:
+        sizes[name] = 0
+
     for name in names:
         mid = mlog.name_to_id[name]
         count = mlog.counts[mid]
         mlen = mlog.formats[mid].len
-        total_size += count * mlen
-        if name in REPLAY_MSGS:
-            replay_total_size += count * mlen
+        size = count * mlen
+        total_size += size
+        sizes[name] += size
         pairs.append((name, count*mlen))
+
     pairs = sorted(pairs, key = lambda p : p[1])
     print("Total size: %u" % total_size)
     for (name,size) in pairs:
         if size > 0:
             print("%-4s %.2f%%" % (name, 100.0 * size / total_size))
-    print("Replay takes %.2f%%" % (100.0 * replay_total_size / total_size))
+
+    print("")
+    category_total = 0
+    for c in categories.keys():
+        total = 0
+        for p in categories[c]:
+            for name in names:
+                if fnmatch.fnmatch(name, p):
+                    total += sizes[name]
+        category_total += total
+        if total > 0:
+            print("@%s %.2f%%" % (c, 100.0 * total / total_size))
+    print("@OTHER %.2f%%" % (100.0 * (total_size-category_total) / total_size))
 
 for filename in args.logs:
     show_stats(filename)
