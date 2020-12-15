2817638be733abf14e6dfcadc06a511be7b336b1
jblam <jeremy.blamey@gmail.com>
2020-12-15 21:06:11 +1100

    Fixed character encoding issue in copyright

--- generator/C/include_v2.0/mavlink_sha256.h
+++ generator/C/include_v2.0/mavlink_sha256.h
@@ -5,7 +5,7 @@
   modifications to suit mavlink headers
  */
 /*
- * Copyright (c) 1995 - 2001 Kungliga Tekniska H�gskolan
+ * Copyright (c) 1995 - 2001 Kungliga Tekniska Högskolan
  * (Royal Institute of Technology, Stockholm, Sweden).
  * All rights reserved.
  * 
