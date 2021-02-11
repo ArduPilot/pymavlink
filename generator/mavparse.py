29ecdd435c3aa172e4020dea5198d9eaeb46a326
rich.nicholson <richard.nicholson@flylogix.com>
2021-02-11 23:14:13 +0000

    Don't add includes that have .. in their path name

--- generator/mavparse.py
+++ generator/mavparse.py
@@ -314,7 +314,9 @@ class MAVXML(object):
             elif in_element == "mavlink.version":
                 self.version = int(data)
             elif in_element == "mavlink.include":
-                self.include.append(data)
+                # Don't add includes which are elsewhere in the folder structure
+                if  ".." not in data:
+                    self.include.append(data)
 
         f = open(filename, mode='rb')
         p = xml.parsers.expat.ParserCreate()
