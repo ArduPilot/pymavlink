a0178f1c270572e43958bf712041bf50782ecbb2
rich.nicholson <richard.nicholson@flylogix.com>
2021-03-08 17:38:58 +0000

    SOF-512 | Check whether message data contains bytes

--- mavutil.py
+++ mavutil.py
@@ -498,9 +498,8 @@ class mavfile(object):
         while True:
             n = self.mav.bytes_needed()
             s = self.recv(n)
-            if not isinstance(s, bytes):
-               continue
-            packet_bytes += s
+            if isinstance(s, bytes):
+               packet_bytes += s
             numnew = len(s)
 
             if numnew != 0:
@@ -540,9 +539,8 @@ class mavfile(object):
                 if start_time + timeout < time.time():
                     return (None, None)
             m,s = self.fx_recv_msg()
-            if not isinstance(s, bytes):
-                continue
-            packet_bytes += s
+            if isinstance(s, bytes):
+                packet_bytes += s
             if m is None:
                 if blocking:
                     for hook in self.idle_hooks:
