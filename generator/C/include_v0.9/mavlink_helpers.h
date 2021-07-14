b5a5b857750457c7c2eb80b59d2795ffbe60e38c
Peter Barker <pbarker@barker.dropbear.id.au>
2021-07-14 10:40:58 +1000

    generator: remove written-only bufferIndex state from C bindings

--- generator/C/include_v0.9/mavlink_helpers.h
+++ generator/C/include_v0.9/mavlink_helpers.h
@@ -245,7 +245,6 @@ the headers.
 
 	mavlink_message_t* rxmsg = mavlink_get_channel_buffer(chan); ///< The currently decoded message
 	mavlink_status_t* status = mavlink_get_channel_status(chan); ///< The current decode status
-	int bufferIndex = 0;
 
 	status->msg_received = 0;
 
@@ -381,7 +380,6 @@ the headers.
 		break;
 	}
 
-	bufferIndex++;
 	// If a message has been successfully decoded, check index
 	if (status->msg_received == 1)
 	{
