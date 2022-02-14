66d09822b687ec3fea01d7160b656cfcdaac04cd
Andrew Tridgell <andrew@tridgell.net>
2022-02-14 15:35:58 +1100

    run generator from repo, avoiding CI issues

--- test_generator.sh
+++ test_generator.sh
@@ -13,16 +13,14 @@ test -z "$MDEF" && MDEF="../message_definitions"
 
 # MAVLINK_DIALECT=ardupilotmega python setup.py clean build install --user
 
-mavgen.py --lang C $MDEF/v1.0/ardupilotmega.xml -o generator/C/include_v1.0 --wire-protocol=1.0
-mavgen.py --lang C $MDEF/v1.0/ardupilotmega.xml -o generator/C/include_v2.0 --wire-protocol=2.0
-
-mavgen.py --lang C++11 $MDEF/v1.0/ardupilotmega.xml -o generator/CPP11/include_v2.0 --wire-protocol=2.0
+tools/mavgen.py --lang C $MDEF/v1.0/all.xml -o generator/C/include_v1.0 --wire-protocol=1.0
+tools/mavgen.py --lang C $MDEF/v1.0/all.xml -o generator/C/include_v2.0 --wire-protocol=2.0
+tools/mavgen.py --lang C++11 $MDEF/v1.0/all.xml -o generator/CPP11/include_v2.0 --wire-protocol=2.0
 
 pushd generator/C/test/posix
-make clean testmav1.0_common testmav2.0_common testmav1.0_ardupilotmega testmav2.0_ardupilotmega testmav2.0_minimal
+make clean testmav1.0_ardupilotmega testmav2.0_ardupilotmega
+
 # these test tools emit the test packet as hexadecimal and human-readable, other tools consume it as a cross-reference, we ignore the hex here.
-./testmav1.0_common | egrep -v '(^fe|^fd)'
-./testmav2.0_common | egrep -v '(^fe|^fd)'
 ./testmav1.0_ardupilotmega | egrep -v '(^fe|^fd)'
 ./testmav2.0_ardupilotmega | egrep -v '(^fe|^fd)'
 popd
