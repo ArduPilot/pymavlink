e2c9bd33aa8ccb856e60c1f80ac082caec6cce76
Alexander Hedges <alehed@users.noreply.github.com>
2022-04-20 16:48:16 +0200

    Copy over x25crc from mavcrc
    This removes all external dependencies from the generated file.

--- generator/mavgen_python.py
+++ generator/mavgen_python.py
@@ -39,8 +39,6 @@ import sys
 import time
 from builtins import object, range
 
-from pymavlink.generator.mavcrc import x25crc
-
 WIRE_PROTOCOL_VERSION = "${WIRE_PROTOCOL_VERSION}"
 DIALECT = "${DIALECT}"
 
@@ -88,6 +86,39 @@ MAVLINK_TYPE_UINT64_T = 7
 MAVLINK_TYPE_INT64_T = 8
 MAVLINK_TYPE_FLOAT = 9
 MAVLINK_TYPE_DOUBLE = 10
+
+
+class x25crc(object):
+    """CRC-16/MCRF4XX - based on checksum.h from mavlink library"""
+
+    def __init__(self, buf=None):
+        self.crc = 0xFFFF
+        if buf is not None:
+            if isinstance(buf, str):
+                self.accumulate_str(buf)
+            else:
+                self.accumulate(buf)
+
+    def accumulate(self, buf):
+        """add in some more bytes"""
+        accum = self.crc
+        for b in buf:
+            tmp = b ^ (accum & 0xFF)
+            tmp = (tmp ^ (tmp << 4)) & 0xFF
+            accum = (accum >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)
+        self.crc = accum
+
+    def accumulate_str(self, buf):
+        """add in some more bytes"""
+        accum = self.crc
+        bytes_array = array.array("B")
+        try:  # if buf is bytes
+            bytes_array.frombytes(buf)
+        except TypeError:  # if buf is str
+            bytes_array.frombytes(buf.encode())
+        except AttributeError:  # Python < 3.2
+            bytes_array.fromstring(buf)
+        self.accumulate(bytes_array)
 
 
 # swiped from DFReader.py
