b0fcee50e159204b0c3ef1c20e3d5f580237100d
Alexander Hedges <alehed@users.noreply.github.com>
2022-05-17 09:19:17 +0200

    Fix test to assume mavlink 2

--- tests/test_trim.py
+++ tests/test_trim.py
@@ -19,15 +19,15 @@ class PayLoadTrimZeros(unittest.TestCase):
         mavutil.mavlink.WIRE_PROTOCOL_VERSION = 2
         mav = mavutil.mavudp(":12345")
 
-        ts = [ ((1, 1), 10),
-              ((1, 0), 9),
-              ((0, 0), 9)
+        ts = [ ((1, 1), 14),
+              ((1, 0), 13),
+              ((0, 0), 13)
               ]
         for t in ts:
             ((sysid, compid), result) = t
             m = mavutil.mavlink.MAVLink_param_request_list_message(sysid, compid)
             packed = m.pack(mav.mav)
-            print("(%u/%u should be %u" % (sysid,compid, result))
+            print("(%u/%u) should be %u" % (sysid,compid, result))
             self.assertEqual(len(packed), result)
 
 if __name__ == '__main__':
