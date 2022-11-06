bc9fb02dad1c5fb331860f8ff2f304876c818425
Alexander Hedges <alehed@users.noreply.github.com>
2022-11-06 18:35:34 +0100

    Simplify x25crc type handling

--- generator/mavcrc.py
+++ generator/mavcrc.py
@@ -4,37 +4,38 @@ MAVLink CRC-16/MCRF4XX code
 Copyright Andrew Tridgell
 Released under GNU LGPL version 3 or later
 '''
+import sys
 from builtins import object
 
 
 class x25crc(object):
-    '''CRC-16/MCRF4XX - based on checksum.h from mavlink library'''
+    """CRC-16/MCRF4XX - based on checksum.h from mavlink library"""
+
     def __init__(self, buf=None):
-        self.crc = 0xffff
+        self.crc = 0xFFFF
         if buf is not None:
-            if isinstance(buf, str):
-                self.accumulate_str(buf)
-            else:
-                self.accumulate(buf)
+            self.accumulate(buf)
 
     def accumulate(self, buf):
-        '''add in some more bytes'''
+        """add in some more bytes (it also accepts strings)"""
+        if sys.version_info[0] == 2:
+            if type(buf) is str:
+                buf = bytearray(buf)
+            elif type(buf).__name__ == 'unicode':
+                # we can't use the identifier unicode in python3
+                buf = bytearray(buf.encode())
+        elif type(buf) is str:
+            buf = buf.encode()
+
         accum = self.crc
         for b in buf:
-            tmp = b ^ (accum & 0xff)
-            tmp = (tmp ^ (tmp<<4)) & 0xFF
-            accum = (accum>>8) ^ (tmp<<8) ^ (tmp<<3) ^ (tmp>>4)
+            tmp = b ^ (accum & 0xFF)
+            tmp = (tmp ^ (tmp << 4)) & 0xFF
+            accum = (accum >> 8) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)
         self.crc = accum
 
     def accumulate_str(self, buf):
-        '''add in some more bytes'''
-        accum = self.crc
-        import array
-        bytes_array = array.array('B')
-        try:  # if buf is bytes
-            bytes_array.frombytes(buf)
-        except TypeError:  # if buf is str
-            bytes_array.frombytes(buf.encode())
-        except AttributeError:  # Python < 3.2
-            bytes_array.fromstring(buf)
-        self.accumulate(bytes_array)
+        """
+        Provided for backwards compatibility. accumulate now also works on strings.
+        """
+        return self.accumulate(buf)
