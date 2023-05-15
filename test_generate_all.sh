7a44a68308fdd07ad31a15901e08b404158530df
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2023-05-15 16:06:47 +1000

    add mavgen_lua to the test_generate_all.sh script

--- test_generate_all.sh
+++ test_generate_all.sh
@@ -14,6 +14,8 @@ mavgen.py --lang='WLua'       --output=/tmp/mavgen_test mavlink/message_definiti
 mavgen.py --lang='ObjC'       --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=1.0 --strict-units
 mavgen.py --lang='Swift'      --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=1.0 --strict-units
 mavgen.py --lang='Java'       --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=1.0 --strict-units
+mavgen.py --lang='Lua'       --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=1.0 --strict-units
+
 
 echo "Test Generate ALL in mavlink v2"
 mavgen.py --lang='C'          --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=2.0 --strict-units
@@ -27,3 +29,4 @@ mavgen.py --lang='ObjC'       --output=/tmp/mavgen_test mavlink/message_definiti
 mavgen.py --lang='Swift'      --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=2.0 --strict-units
 mavgen.py --lang='Java'       --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=2.0 --strict-units
 mavgen.py --lang='C++11'      --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=2.0 --strict-units # C++11 generator only supports 2.0
+mavgen.py --lang='Lua'       --output=/tmp/mavgen_test mavlink/message_definitions/v1.0/common.xml --wire-protocol=2.0 --strict-units
