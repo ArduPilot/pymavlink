e5eaa317db79a7c714c689ef453554f336cf91d8
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2024-01-22 21:40:21 +0100

    Fix condition data discarding when --meta is set

--- tools/mavlogdump.py
+++ tools/mavlogdump.py
@@ -136,7 +136,7 @@ def reduce_rate_msg(m, reduction_rate):
     '''return True if this msg should be discarded by reduction'''
     global last_msg_rate_t
     mtype = m.get_type()
-    if mtype in ['PARM','MSG','FMT','FMTU','MULT','MODE','EVT']:
+    if mtype in ['PARM','MSG','FMT','FMTU','MULT','MODE','EVT','UNIT']:
         return False
     t = getattr(m,'_timestamp',None)
     if t is None:
@@ -273,7 +273,7 @@ while True:
             continue
 
     if not mavutil.evaluate_condition(args.condition, mlog.messages) and (
-            not (m_type in ['FMT', 'FMTU', 'MULT','PARM','MODE'] and args.meta)):
+            not (m_type in ['FMT', 'FMTU', 'MULT', 'PARM', 'MODE', 'UNIT'] and args.meta)):
         continue
     if args.source_system is not None and args.source_system != m.get_srcSystem():
         continue
