7ec42487f62eb07d8542f873d14eea8148ae6b7c
Dr.-Ing. Amilcar do Carmo Lucas <amilcar.lucas@iav.de>
2024-09-19 01:38:15 +0200

    pylint fixes to mavftp

--- mavftp_op.py
+++ mavftp_op.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+MAVLink File Transfer Protocol support - https://mavlink.io/en/services/ftp.html
+
+SPDX-FileCopyrightText: 2011-2024 Andrew Tridgell, 2024 Amilcar Lucas
+
+SPDX-License-Identifier: GPL-3.0-or-later
+'''
 
 import struct
 from typing import Optional
 
 
+# pylint: disable=invalid-name
 OP_None = 0
 OP_TerminateSession = 1
 OP_ResetSessions = 2
@@ -22,12 +32,14 @@ OP_BurstReadFile = 15
 OP_Ack = 128
 OP_Nack = 129
 
+# pylint: enable=invalid-name
 
 # pylint: disable=too-many-arguments
 # pylint: disable=too-many-instance-attributes
 
 
-class FTP_OP:
+class FTP_OP:  # pylint: disable=invalid-name
+    """FTP operation"""
     def __init__(
         self,
         seq: int,
@@ -70,7 +82,19 @@ class FTP_OP:
         plen = 0
         if self.payload is not None:
             plen = len(self.payload)
-        ret = f"OP seq:{self.seq} sess:{self.session} opcode:{self.opcode} req_opcode:{self.req_opcode} size:{self.size} bc:{self.burst_complete} ofs:{self.offset} plen={plen}"
+        ret = f"OP seq:{self.seq} sess:{self.session} opcode:{self.opcode} req_opcode:{self.req_opcode} size:{self.size}" \
+              f" bc:{self.burst_complete} ofs:{self.offset} plen={plen}"
         if self.payload is not None and plen > 0:
             ret += f" [{self.payload[0]}]"
         return ret
+
+    def items(self):
+        """Yield each attribute and its value for the FTP_OP instance. For debugging purposes."""
+        yield "seq", self.seq
+        yield "session", self.session
+        yield "opcode", self.opcode
+        yield "size", self.size
+        yield "req_opcode", self.req_opcode
+        yield "burst_complete", self.burst_complete
+        yield "offset", self.offset
+        yield "payload", self.payload
