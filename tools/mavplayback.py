8591fa79f05a75ebf9d1f0863bc9315c49e7c97b
Andrew Tridgell <andrew@tridgell.net>
2025-01-29 12:26:31 +1100

    mark all tools as python3

--- tools/mavplayback.py
+++ tools/mavplayback.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 '''
 play back a mavlink log as a FlightGear FG NET stream, and as a
--- tools/mavsigloss.py
+++ tools/mavsigloss.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 '''
 show times when signal is lost
