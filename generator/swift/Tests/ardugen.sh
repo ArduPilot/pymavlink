ce3181f02df0255eb3e494b8b2a24bca6f854db9
Peter Barker <pbarker@barker.dropbear.id.au>
2025-05-27 01:51:43 +1000

    Correct Python3 invocations to python3 not python
    also use "python3 -m pip" rather than just "pip"

--- generator/swift/Tests/ardugen.sh
+++ generator/swift/Tests/ardugen.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH="$PYTHONPATH:../../../../"
 
 rm -rf ./MAVLink/MAVLink/Swift/
-python -m pymavlink.tools.mavgen ./MAVLinkTests/Testdata/ardupilotmega.xml -o ./MAVLink/MAVLink/Swift/ --wire-protocol 1.0 --lang Swift
+python3 -m pymavlink.tools.mavgen ./MAVLinkTests/Testdata/ardupilotmega.xml -o ./MAVLink/MAVLink/Swift/ --wire-protocol 1.0 --lang Swift
 
 rm -rf ./MAVLinkTests/MAVLink/C/
-python -m pymavlink.tools.mavgen ./MAVLinkTests/Testdata/ardupilotmega.xml -o ./MAVLinkTests/MAVLink/C/ --wire-protocol 1.0 --lang C
+python3 -m pymavlink.tools.mavgen ./MAVLinkTests/Testdata/ardupilotmega.xml -o ./MAVLinkTests/MAVLink/C/ --wire-protocol 1.0 --lang C
