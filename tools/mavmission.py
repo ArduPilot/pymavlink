6eb35a8d55da35e0892c0fa90cd1a71dbe608863
Peter Barker <pbarker@barker.dropbear.id.au>
2025-06-20 21:02:50 +1000

    mavmission: rewrite as a library with a __main__
    to allow for reuse from elsewhere

--- tools/mavmission.py
+++ tools/mavmission.py
@@ -2,76 +2,94 @@
 
 '''
 extract mavlink mission from log
+
+AP_FLAKE8_CLEAN
 '''
-from argparse import ArgumentParser
-parser = ArgumentParser(description=__doc__)
-parser.add_argument("--output", default='mission.txt', help="output file")
-parser.add_argument("logs", metavar="LOG", nargs="+")
+from pymavlink import mavutil
+from pymavlink import mavwp
 
-args = parser.parse_args()
+class MAVMission():
+    def __init__(self, filepath : str) -> None:
+        self.filepath : str = filepath
 
-from pymavlink import mavutil, mavwp
+    def run(self) -> mavwp.MAVWPLoader:
+        '''parses the log, returns a MAVWPLoader primed with content'''
+        mlog = mavutil.mavlink_connection(self.filepath)
 
-parms = {}
+        wp = mavwp.MAVWPLoader()
 
-def mavmission(logfile):
-    '''extract mavlink mission'''
-    mlog = mavutil.mavlink_connection(filename)
+        num_wps = None
+        while True:
+            m = mlog.recv_match(type=set([
+                'MISSION_ITEM',
+                'CMD',
+                'WAYPOINT',
+                'MISSION_ITEM_INT',
+            ]))
+            if m is None:
+                break
+            if m.get_type() == 'CMD':
+                try:
+                    frame = m.Frame
+                except AttributeError:
+                    print("Warning: assuming frame is GLOBAL_RELATIVE_ALT")
+                    frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
+                num_wps = m.CTot
+                m = mavutil.mavlink.MAVLink_mission_item_message(
+                    0,
+                    0,
+                    m.CNum,
+                    frame,
+                    m.CId,
+                    0, 1,
+                    m.Prm1, m.Prm2, m.Prm3, m.Prm4,
+                    m.Lat, m.Lng, m.Alt
+                )
 
-    wp = mavwp.MAVWPLoader()
+            if m.get_type() == 'MISSION_ITEM_INT':
+                m = mavutil.mavlink.MAVLink_mission_item_message(
+                    m.target_system,
+                    m.target_component,
+                    m.seq,
+                    m.frame,
+                    m.command,
+                    m.current,
+                    m.autocontinue,
+                    m.param1,
+                    m.param2,
+                    m.param3,
+                    m.param4,
+                    m.x*1.0e-7,
+                    m.y*1.0e-7,
+                    m.z,
+                )
+            if m.current >= 2:
+                continue
 
-    num_wps = None
-    while True:
-        m = mlog.recv_match(type=['MISSION_ITEM','CMD','WAYPOINT','MISSION_ITEM_INT'])
-        if m is None:
-            break
-        if m.get_type() == 'CMD':
-            try:
-                frame = m.Frame
-            except AttributeError:
-                print("Warning: assuming frame is GLOBAL_RELATIVE_ALT")
-                frame = mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT
-            num_wps = m.CTot
-            m = mavutil.mavlink.MAVLink_mission_item_message(0,
-                                                             0,
-                                                             m.CNum,
-                                                             frame,
-                                                             m.CId,
-                                                             0, 1,
-                                                             m.Prm1, m.Prm2, m.Prm3, m.Prm4,
-                                                             m.Lat, m.Lng, m.Alt)
+            while m.seq > wp.count():
+                print("Adding dummy WP %u" % wp.count())
+                wp.set(m, wp.count())
+            wp.set(m, m.seq)
 
-        if m.get_type() == 'MISSION_ITEM_INT':
-            m = mavutil.mavlink.MAVLink_mission_item_message(m.target_system,
-                                                             m.target_component,
-                                                             m.seq,
-                                                             m.frame,
-                                                             m.command,
-                                                             m.current,
-                                                             m.autocontinue,
-                                                             m.param1,
-                                                             m.param2,
-                                                             m.param3,
-                                                             m.param4,
-                                                             m.x*1.0e-7,
-                                                             m.y*1.0e-7,
-                                                             m.z)
-        if m.current >= 2:
-            continue
+        if num_wps is not None:
+            while wp.count() > num_wps:
+                wp.remove(wp.wp(wp.count() - 1))
 
-        while m.seq > wp.count():
-            print("Adding dummy WP %u" % wp.count())
-            wp.set(m, wp.count())
-        wp.set(m, m.seq)
+        return wp
 
-    if num_wps is not None:
-        while wp.count() > num_wps:
-            wp.remove(wp.wp(wp.count() - 1))
+def main() -> None:
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument("--output", default='mission.txt', help="output file")
+    parser.add_argument("log", metavar="LOG")
+
+    args = parser.parse_args()
+
+    mm = MAVMission(args.log)
+    wp = mm.run()
 
     wp.save(args.output)
     print("Saved %u waypoints to %s" % (wp.count(), args.output))
 
-
-total = 0.0
-for filename in args.logs:
-    mavmission(filename)
+if __name__ == '__main__':
+    main()
